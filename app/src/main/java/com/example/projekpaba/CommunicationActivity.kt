a1e3ffa2203f1b288a8005addf3a8b815975b138
nadya <nadya.sutanto@gmail.com>
2024-12-26 01:09:14 +0700

    ubah string deskripsi

--- app/src/main/java/com/example/projekpaba/CommunicationActivity.kt
+++ app/src/main/java/com/example/projekpaba/CommunicationActivity.kt
@@ -1,13 +1,20 @@
 package com.example.projekpaba
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import com.squareup.picasso.Picasso
 
 class CommunicationActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_communication)
@@ -22,12 +29,21 @@ class CommunicationActivity : AppCompatActivity() {
             finish()
         }
 
-        val dataIntent = intent.getParcelableExtra<agencyMarketing>("kirimData", agencyMarketing::class.java)
-        if (dataIntent != null) {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val dataIntentDetail = intent.getParcelableExtra<agencyMarketing>("kirimData", agencyMarketing::class.java)
+        if (dataIntentDetail != null) {
             Picasso.get()
-                .load(dataIntent.foto)
+                .load(dataIntentDetail.foto)
                 .into(_ivLogo)
-            _tvNameAgency.setText(dataIntent.nama)
+            Picasso.get()
+                .load(dataIntentDetail.foto)
+                .into(_ivLogoMini)
+            _tvNameAgency.setText(dataIntentDetail.nama)
         }
     }
 }
