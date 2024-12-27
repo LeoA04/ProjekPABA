ed90d781fa294f1c251c4c73d3460004345c5fc0
fransiskaff <146209394+fransiskaff@users.noreply.github.com>
2024-12-27 12:30:52 +0700

    dashboard

---
+++ app/src/main/java/com/example/projekpaba/dashboardPage.kt
@@ -0,0 +1,20 @@
+package com.example.projekpaba
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class dashboardPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
