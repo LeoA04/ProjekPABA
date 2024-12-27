4e776d81db33f0ea53154f282a629752e3318523
leonard <c14220276@john.petra.ac.id>
2024-12-27 16:50:57 +0700

    firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.kotlinParcelize) apply false
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
