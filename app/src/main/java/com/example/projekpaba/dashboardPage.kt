e9d01687015db7f8d0b9a27e74fb6d4c55bde083
fransiskaff <146209394+fransiskaff@users.noreply.github.com>
2024-12-27 23:45:37 +0700

    dashboard

--- app/src/main/java/com/example/projekpaba/dashboardPage.kt
+++ app/src/main/java/com/example/projekpaba/dashboardPage.kt
@@ -1,8 +1,12 @@
 package com.example.projekpaba
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,10 +23,34 @@ class dashboardPage : AppCompatActivity() {
             insets
         }
         //button see all recommendation
-        val btnSeeRecommendation = findViewById<Button>(R.id.btnSeeRecommendation)
+        val btnSeeRecommendation = findViewById<TextView>(R.id.btnSeeRecommendation)
         btnSeeRecommendation.setOnClickListener {
             val intent = Intent(this, RecommendationActivity::class.java)
             startActivity(intent)
         }
+
+//        val _ivProfile = findViewById<ImageView>(R.id.ivProfile)
+//        _ivProfile.setOnClickListener {
+//            val intent = Intent(this, ProfileActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        val _historySection = findViewById<LinearLayout>(R.id.history_section)
+        _historySection.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        val _voucherSection = findViewById<LinearLayout>(R.id.voucher_section)
+        _voucherSection.setOnClickListener {
+
+        }
+
+//        val _ivNotification = findViewById<ImageView>(R.id.ivNotification)
+//        _ivNotification.setOnClickListener {
+//
+//        }
+
+
     }
 }
